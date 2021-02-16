0af560c3b6973b84034c9a3f96192dcba02b8c14
FlapCZ <69921885+FlapCZ@users.noreply.github.com>
2021-02-16 02:03:20 +0100

    Update sv_config.lua

--- config/sv_config.lua
+++ config/sv_config.lua
@@ -5,8 +5,8 @@ Config              = {}
 Config.Locale = 'en'
 
 Config.general_server_settings = {
-	oldESX              = false,
-	esx                 = 'hsuiwsdSAD587:479582sqeqesd_dbcuwq', -- default : esx:getSharedObject
+	oldESX              = false, -- true if you are using es_extended 1.1 and smaller
+	esx                 = 'esx:getSharedObject', -- default : esx:getSharedObject
 	RequiredCopsCraft   = 0,
 	RequiredCopsPackage = 0,
 }
